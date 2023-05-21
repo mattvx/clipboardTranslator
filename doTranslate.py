89bf2067f2bfa842a8d14315785f292d6ca6b76f
Matteo <93927786+mattvx@users.noreply.github.com>
2023-05-21 12:35:12 +0200

    Create doTranslate.py

---
+++ doTranslate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+try:
+    import deepl, argparse, clipboard
+except ModuleNotFoundError as e:
+    print(f"One or more modules not found! Please install module {e.name}")
+    exit(1)
+
+# Argparse Stuff
+parser = argparse.ArgumentParser()
+parser.add_argument("--auth-key", dest="authkey", required=True)
+parser.add_argument("--dest-lang", dest="lang", required=True)
+args = parser.parse_args()
+
+# Auth Key, clipboard and target language parsing
+auth_key = args.authkey
+text = clipboard.paste()
+target_language = args.lang
+
+# Translating
+translator = deepl.Translator(auth_key) 
+result = translator.translate_text(text, target_lang=target_language) 
+translated_text = result.text
+
+# Copying back translated stuff to clipboard
+clipboard.copy(translated_text)
